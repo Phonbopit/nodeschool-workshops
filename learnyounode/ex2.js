5d67988ef3ac7f67262131591ae9834b7644c183
Chai Phonbopit <phonbopit@gmail.com>
2014-11-28 11:54:36 +0700

    add exercise 4

--- learnyounode/ex2.js
+++ learnyounode/ex2.js
@@ -1,3 +1,10 @@
+// LEARN YOU THE NODE.JS FOR MUCH WIN!
+// Ex.2 : BABY STEPS
+
+// Write a program that accepts one or more numbers as command-line arguments 
+// and prints the sum of those numbers to the console (stdout).
+
+
 var inputs = process.argv;
 
 var total = 0;
@@ -9,3 +16,11 @@ inputs.forEach(function (data) {
 });
 
 console.log(total);
+
+
+// Note 
+// - `process.argv`, contain a array from command line.
+// - All elements of `process.arvs` are strings. 
+// - `process.argv[0]` is always 'node'
+// - `process-argv[1]` is always the path to program.
+// - Use `Number()` to passing a String to Number.
